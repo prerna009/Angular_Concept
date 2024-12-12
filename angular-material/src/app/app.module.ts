ce8e41cb798b4ed6a0caca8c580b06b78789c3da
prernagupta <prerna.gupta@nimapinfotech.com>
2024-12-12 18:34:54 +0530

    Add changes

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatInputModule } from '@angular/material/input';
@@ -16,6 +19,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +28,9 @@ import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
     AComponent,
     BComponent,
     AngularMaterialComponent,
-    BottomSheetComponent
+    BottomSheetComponent,
+    ButtonComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +43,10 @@ import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
     ReactiveFormsModule,
     MatSlideToggleModule,
     MatBadgeModule,
-    MatBottomSheetModule
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule
   ],
   providers: [
     provideClientHydration()
---
+++ angular-material/src/app/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  selectedValue='';
+  alertsEnabled=false;
+}
