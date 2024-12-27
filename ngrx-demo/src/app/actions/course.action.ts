f2b2ef7fb8791735f0fd6a47a8abc2a13a2e7a2d
prernagupta <prerna.gupta@nimapinfotech.com>
2024-12-27 11:00:56 +0530

    Perform ngrx

--- ngrx-demo/src/app/actions/course.action.ts
+++ ngrx-demo/src/app/actions/course.action.ts
@@ -5,10 +5,22 @@ export const loadCourses = createAction('[Course] Load Courses');
 
 export const loadCoursesSuccess = createAction('[Course] Load Courses Success', props<{ courses: Course[] }>());
 
-export const loadCoursesFailure = createAction('[Course] Load Courses Failure',props<{ error: any }>());  
+export const loadCoursesFailure = createAction('[Course] Load Courses Failure',props<{ error: string }>());  
 
 export const addCourse = createAction('[Course] Add Course', props<{ course: Course }>());
 
+export const addCourseSuccess = createAction('[Course] Add Course Success', props<{ course: Course }>());
+
+export const addCourseFailure = createAction('[Course] Add Course Failure', props<{ error :string }>());
+
 export const updateCourse = createAction('[Course] Update Course', props<{ course: Course }>());
 
+export const updateCourseSuccess = createAction('[Course] Update Course Success', props<{ course: Course }>());
+
+export const updateCourseFailure = createAction('[Course] Update Course Failure', props<{ error:string }>());
+
 export const deleteCourse = createAction('[Course] Delete Course', props<{ id: number }>());
+
+export const deleteCourseSuccess = createAction('[Course] Delete Course', props<{ id: number }>());
+
+export const deleteCourseFailure = createAction('[Course] Delete Course', props<{ error:string }>());
--- ngrx-demo/src/app/component/course-details/course-details.component.ts
+++ ngrx-demo/src/app/component/course-details/course-details.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../../models/course';
 import { Store } from '@ngrx/store';
-import { addCourse, deleteCourse, loadCourses, updateCourse } from '../../actions/course.action';
-import { selectCourses, selectError, selectLoading } from '../../selector/course.selector';
+import * as courseActions from '../../actions/course.action';
+import * as courseSelectors from '../../selector/course.selector';
+import { State } from '../../reducers/course.reducer';
+import { CourseService } from '../../service/course.service';
 
 @Component({
   selector: 'app-course-details',
@@ -11,10 +13,10 @@ import { selectCourses, selectError, selectLoading } from '../../selector/course
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.css'
 })
-export class CourseDetailsComponent implements OnInit {
-  courses$!: Observable<Course[]>;
-  loading$!: Observable<boolean> ;
-  error$!: Observable<string | null>;
+export class CourseDetailsComponent implements OnInit{
+  courses$: Observable<Course[]>;
+  loading$: Observable<boolean> ;
+  error$: Observable<string | null>;
   course: Course = {
     id: 0, 
     title: '',
@@ -23,26 +25,71 @@ export class CourseDetailsComponent implements OnInit {
     price: 0
   };
 
-  constructor(private store: Store) { 
-    this.courses$=this.store.select(selectCourses);
-    this.loading$=this.store.select(selectLoading);
-    this.error$=this.store.select(selectError);
+  constructor(private store: Store<State>,private courseService:CourseService) {
+    this.courses$ = this.store.select(courseSelectors.selectCourses);
+    this.loading$ = this.store.select(courseSelectors.selectLoading);
+    this.error$ = this.store.select(courseSelectors.selectError);
   }
 
   ngOnInit(): void {
-    this.store.dispatch(loadCourses());
+    this.loadCourse();
+  }
+
+  loadCourse():void{
+    this.courseService.getCourses().subscribe({
+      next:(courses)=>{
+        this.store.dispatch(courseActions.loadCoursesSuccess({courses}));
+      },
+      error:(error)=>{
+        this.store.dispatch(courseActions.loadCoursesFailure({error:error.message}));
+      }
+    })
   }
 
   addCourse(course: Course) :void{
-    this.store.dispatch(addCourse({course}));
+    this.courseService.addCourse(course).subscribe({
+      next:(addCourse)=>{
+        this.store.dispatch(courseActions.addCourseSuccess({course:addCourse}));
+        this.loadCourse();
+      },
+      error:(error)=>{
+        this.store.dispatch(courseActions.addCourseFailure({error}));
+      }
+    });
   }
 
   updateCourse(course: Course):void {
-    this.store.dispatch(updateCourse({course}));
+    this.courseService.updateCourse(course).subscribe({
+      next:(updateCourse)=>{
+        this.store.dispatch(courseActions.updateCourseSuccess({course:updateCourse}));
+      },
+      error:(error)=>{
+        this.store.dispatch(courseActions.updateCourseFailure({error}));
+      }
+    });
   }
 
-  deleteCourse(id: number) :void{
-   this.store.dispatch(deleteCourse({id}));
+  deleteCourse(id: number){
+   this.courseService.deleteCourse(id).subscribe({
+    next:()=>{
+      this.store.dispatch(courseActions.deleteCourseSuccess({id}));
+    },
+    error:(error)=>{
+      this.store.dispatch(courseActions.deleteCourseFailure({error}));
+    }
+   });
+  }
+
+  onSubmit(courseForm:any):void{
+    if(this.course.id){
+      this.updateCourse(this.course);
+    } else{
+      this.addCourse(this.course);
+    }
+  }
+
+  onEdit(course:Course):void{
+    this.course={...course};
   }
 }
 
